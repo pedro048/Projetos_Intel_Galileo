1d761a539d5b01a598a12958005a36420424da13
Pedro Victor Andrade Alves <37122281+pedro048@users.noreply.github.com>
2019-05-25 06:25:54 -0300

    Update super_importante.py

--- python/super_importante.py
+++ python/super_importante.py
@@ -3,10 +3,10 @@ import threading
 import time
 
 #---- configuracoes de ligar e desligar ----
-gpio_1 = mraa.Gpio(2)
-gpio_1.dir(mraa.DIR_IN) # botao de ligar
-gpio_2 = mraa.Gpio(4)
-gpio_2.dir(mraa.DIR_IN) # boatao de desligar
+botao_ligar = mraa.Gpio(2)
+botao_ligar.dir(mraa.DIR_IN) # botao de ligar
+botao_desligar = mraa.Gpio(4)
+botao_desligar.dir(mraa.DIR_IN) # boatao de desligar
 liga = 0
 desliga = 0
 aux = 0
@@ -15,14 +15,13 @@ aux = 0
 PWM_PIN = 5
 ADC_PIN = 0           # Analog in pin
 ROT_MAX = 1024.0      # Max value as measured by ADC when pot is connected
-#value = 0
 
-gpio_3 = mraa.Gpio(7) # LED verde
-gpio_3.dir(mraa.DIR_OUT)
-gpio_4 = mraa.Gpio(8) # LED amarelo
-gpio_4.dir(mraa.DIR_OUT)
-gpio_5 = mraa.Gpio(12)# LED vermelho
-gpio_5.dir(mraa.DIR_OUT)
+led_verde = mraa.Gpio(7) # LED verde
+led_verde.dir(mraa.DIR_OUT)
+led_amarelo = mraa.Gpio(8) # LED amarelo
+led_amarelo.dir(mraa.DIR_OUT)
+led_vermelho = mraa.Gpio(12)# LED vermelho
+led_vermelho.dir(mraa.DIR_OUT)
 
 # Set up the PWM
 pwm = mraa.Pwm(PWM_PIN)
@@ -33,8 +32,8 @@ pwm.period_us(5000)
 adc = mraa.Aio(ADC_PIN)
 
 #---- configuracao do BUZZER ----
-gpio_6 = mraa.Gpio(11)
-gpio_6.dir(mraa.DIR_OUT)
+buzzer = mraa.Gpio(11)
+buzzer.dir(mraa.DIR_OUT)
 
 def thread_ADC_PWM(ROT_MAX): 
     
@@ -42,18 +41,18 @@ def thread_ADC_PWM(ROT_MAX):
     led_intensity = value/ROT_MAX  # determina o duty cycle baseado em value 
     pwm.write(led_intensity)
 
-def time_1(): 
+def time_1(): # conta 2s
     
     time.sleep(2)
 
-def time_2(): 
+def time_2(): # conta 0.2s
     
     time.sleep(0.2)
 
 while True: 
 	# ler o estado dos botoes de ligar e desligar
-	liga = gpio_1.read()
-	desliga = gpio_2.read()
+	liga = botao_ligar.read()
+	desliga = botao_desligar.read()
 	if liga == 1: 
 		aux = 1
 
@@ -66,48 +65,48 @@ while True:
 		t1.join()
 		
 		if adc.read() >= 0 and adc.read() < 100:
-			gpio_3.write(1) # liga LED verde (ausencia de incendio)
-			gpio_4.write(0)
-			gpio_5.write(0)
+			led_verde.write(1) # liga LED verde (ausencia de incendio)
+			led_amarelo.write(0)
+			led_vermelho.write(0)
 			# alarme desligado
-			gpio_6.write(0)
+			buzzer.write(0)
 	
 		if adc.read() >= 100 and adc.read() < 200:
-			gpio_3.write(0)
-			gpio_4.write(1) # liga LED amarelo (incendio com intensidade moderada, mas preocupante)
-			gpio_5.write(0)
+			led_verde.write(0)
+			led_amarelo.write(1) # liga LED amarelo (incendio com intensidade moderada, mas preocupante)
+			led_vermelho.write(0)
 			# alarme de incendio moderado
 			t2 = threading.Thread(target=time_1,args=()) 
 			t2.start() 
 			t2.join()
-			gpio_6.write(1)
+			buzzer.write(1)
 			t3 = threading.Thread(target=time_2,args=()) 
 			t3.start() 
 			t3.join()
-			gpio_6.write(0)
+			buzzer.write(0)
 	
 		if adc.read() >= 200 and adc.read() < 1024:
-			gpio_3.write(0)
-			gpio_4.write(0)
-			gpio_5.write(1) # liga LED vermelho (incendio intenso)
+			led_verde.write(0)
+			led_amarelo.write(0)
+			led_vermelho.write(1) # liga LED vermelho (incendio intenso)
 			# alarme de incendio intenso
 			t3 = threading.Thread(target=time_2,args=()) 
 			t3.start() 
 			t3.join()
-			gpio_6.write(1)
+			buzzer.write(1)
 			t3 = threading.Thread(target=time_2,args=()) 
 			t3.start() 
 			t3.join()
-			gpio_6.write(0)
+			buzzer.write(0)
     
 	else:	# sistema desacionado
 	
 		# vazao de agua cortada
 		pwm.write(0) 
 		# LEDs indicadores de intensidade de incendio desligados
-		gpio_3.write(0) # LED verde deligado
-		gpio_4.write(0)	# LED amarelo desligado
-		gpio_5.write(0)	# LED vermelho desligado
+		led_verde.write(0) # LED verde deligado
+		led_amarelo.write(0)	# LED amarelo desligado
+		led_vermelho.write(0)	# LED vermelho desligado
 		# alarme desligado
-		gpio_6.write(0)
+		buzzer.write(0)
 	 
